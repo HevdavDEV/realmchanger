af4a1ff9e04243fc6adb10c42fb7c843f5778069
sdyess <sdyessdev@gmail.com>
2013-06-19 20:18:49 -0500

    Loading from XML is now working, fixes some exceptions

--- Realm Changer/Form1.cs
+++ Realm Changer/Form1.cs
@@ -76,24 +76,38 @@ namespace Realm_Changer
             {
                 XmlDocument doc = new XmlDocument();
                 doc.Load("ServerInfo.xml");
-                XmlNodeList RealmNodes = doc.SelectNodes("Name");
-                XmlNodeList RealmlistNodes = doc.SelectNodes("Realmlist");
-                List<String> Realmlist = new List<String>();
-                List<String> Realms = new List<String>();
+                XmlNodeList RealmNodes = doc.SelectNodes("ServerInfo/Realm/Name");
+                XmlNodeList RealmlistNodes = doc.SelectNodes("ServerInfo/Realm/Realmlist");
+                List<string> Realmlist = new List<string>();
+                List<string> Realms = new List<string>();
 
                 foreach(XmlNode temp_realmlist in RealmlistNodes)
                 {
-                    Realmlist.Add(temp_realmlist.Value.ToString());
+                    Realmlist.Add(temp_realmlist.InnerText);
                 }
 
                 foreach (XmlNode Realm in RealmNodes)
                 {
-                    Realms.Add(Realm.Value.ToString());
+                    Realms.Add(Realm.InnerText);
                 }
 
                 for (int i = 0; i < Realms.Count(); i++)
                 {
-                    ComboBoxList.Add(Realms[i], Realmlist[i]);
+                    try
+                    {
+                        ComboBoxList.Add(Realms[i], Realmlist[i]);
+                    }
+                    catch (ArgumentException)
+                    {
+                    }
+                }
+
+                if (comboBox1.Items.Count > 0)
+                    comboBox1.Items.Clear();
+
+                foreach (KeyValuePair<string, string> entry in ComboBoxList)
+                {
+                    comboBox1.Items.Add(entry.Key);
                 }
             
             }
@@ -108,10 +122,11 @@ namespace Realm_Changer
         private void button1_Click_1(object sender, EventArgs e)
         {
             //delete button
-            XmlDocument doc = new XmlDocument();
-            doc.Load("ServerInfo.xml");
+            
             try
             {
+                XmlDocument doc = new XmlDocument();
+                doc.Load("ServerInfo.xml");
                 string path = "ServerInfo/Realm[Name='" + comboBox1.SelectedItem.ToString() + "']";
                 XmlNode node = doc.SelectSingleNode(path);
             }
@@ -125,6 +140,6 @@ namespace Realm_Changer
             LoadXmlFile();
         }
 
-        private Dictionary<String, String> ComboBoxList;
+        private Dictionary<string, string> ComboBoxList = new Dictionary<string,string>();
     }
 }
